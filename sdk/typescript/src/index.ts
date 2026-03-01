85d471cacad73a8502534982e189e25afb53e861
Sushant <sam1064max@gmail.com>
2026-03-01 11:41:00 +0530

    feat: add TypeScript SDK index with type exports

---
+++ sdk/typescript/src/index.ts
@@ -0,0 +1,14 @@
+export { AgentLoopClient } from "./client";
+export { Trace } from "./trace";
+export type { TraceData } from "./trace";
+export { Outcome } from "./outcome";
+export type { OutcomeData, OutcomeType } from "./outcome";
+export { Feedback } from "./feedback";
+export type { FeedbackData } from "./feedback";
+export { Experiment } from "./experiment";
+export type { ExperimentData } from "./experiment";
+export { Version } from "./version";
+export type { VersionData, VersionType } from "./version";
+export { traceAgent, trackOutcome } from "./decorators";
+
+# history: feat: add TypeScript SDK index with type exports
